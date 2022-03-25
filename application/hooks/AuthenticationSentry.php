2aa3fb5a347fbe892835f5775028517185d26310
Calvin Roger S. Canas <calvinrogercanas@gmail.com>
2022-03-26 06:41:50 +0800

    better auth checker approach

--- application/hooks/AuthenticationSentry.php
+++ application/hooks/AuthenticationSentry.php
@@ -5,30 +5,38 @@ class AuthenticationSentry {
 	/**
 	 * @var CI_Controller
 	 */
-	private $codeIgniterInstance;
+	public $codeIgniterInstance;
+	/**
+	 * @var object
+	 */
+	public $router;
 
 	public function __construct()
     {
-		$this->codeIgniterInstance = &get_instance();
+		$this->codeIgniterInstance =& get_instance();
+		$this->router =& load_class('Router');
     }
 
-	public function isUserLogin()
+	public function isUserLoggedin()
 	{
 //		User check session
 //			You must use hooks to be able to check if current user’s session is
 //			expired or not. If it is expired, user will be redirected to the login
 //			page. If user’s session is still not expired and he tried to access the
 //			login page, he will be redirected to employee’s time record page.
-
+		$this->codeIgniterInstance->load->helper('url');
 		if (!$this->codeIgniterInstance->session->has_userdata('username')) {
-			if (uri_string() !== 'login') {
-				redirect(base_url() . 'login');
+			if (property_exists($this->codeIgniterInstance, 'guestAllowedMethods')) {
+				if (!in_array($this->router->fetch_method(), $this->codeIgniterInstance->guestAllowedMethods, false)) {
+					redirect(base_url('login/index'));
+				}
+			} else {
+				redirect(base_url('login/index'));
 			}
-		}
-
-		// if logged in and trying to access login page
-		if (uri_string() === 'login') {
-			redirect(base_url('login/success')); // mmya i recode
+		} else {
+			if ('login' == $this->router->fetch_class() && 'index' == $this->router->fetch_method()) {
+				redirect(base_url('employee/index'));
+			}
 		}
 	}
 }
