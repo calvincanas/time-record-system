1ad166c5078e8df7631e557f4ee22437f9069469
Calvin Roger S. Canas <calvinrogercanas@gmail.com>
2022-03-26 04:15:09 +0800

    added Base Code
    - employees crud
    - user can login
    - user can logout
    - some other ci setup

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -20,6 +20,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		require APPPATH . 'libraries/phpqrcode/phpqrcode.php';
+		$wew = QRcode::png('PHP QR Code :)');
+		echo $wew;
+//		$this->load->view('welcome_message');
 	}
 }
